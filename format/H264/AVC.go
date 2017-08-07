71d0d40c8cd6ed67868011c4db789a66090e9aa8
zhouchuanyun <zhouchuanyun@avicomms.com>
2017-08-07 17:56:02 +0800

    8-7

---
+++ format/H264/AVC.go
@@ -0,0 +1,181 @@
+package H264
+
+import (
+	"container/list"
+	"bytes"
+	"errors"
+)
+
+type AVCDecoderConfigurationRecord struct {
+	ConfigurationVersion         byte //aways 1
+	AVCProfileIndication         byte
+	Profile_compatibility        byte
+	AVCLevelIndication           byte
+	LengthSizeMinusOne           byte
+	NumOfSequenceParameterSets   int
+	SPS                          *list.List
+	NumOfPictureParameterSets    int
+	PPS                          *list.List
+	Chroma_format_idc            byte
+	Bit_depth_luma_minus8        byte
+	Bit_depth_chroma_minus8      byte
+	NumOfSequenceParameterSetExt int
+	SequenceParameterSetExt      *list.List
+}
+
+func (this *AVCDecoderConfigurationRecord)AVCData()([]byte){
+	writer:=&bytes.Buffer{}
+	writer.WriteByte(1)
+	writer.WriteByte(this.AVCProfileIndication)
+	writer.WriteByte(this.Profile_compatibility)
+	writer.WriteByte(this.AVCLevelIndication)
+	writer.WriteByte(0xfc|this.LengthSizeMinusOne)
+	writer.WriteByte(0xe0|byte(this.NumOfSequenceParameterSets))
+	writeSizeDataList(writer,this.SPS)
+	writer.WriteByte(byte(this.NumOfPictureParameterSets))
+	writeSizeDataList(writer,this.PPS)
+	if this.AVCProfileIndication==100||this.AVCProfileIndication==110||
+	this.AVCProfileIndication==122||this.AVCProfileIndication==144{
+		writer.WriteByte(0xfc|this.Chroma_format_idc)
+		writer.WriteByte(0xf8|this.Bit_depth_luma_minus8)
+		writer.WriteByte(0xf8|this.Bit_depth_chroma_minus8)
+		writer.WriteByte(byte(this.NumOfSequenceParameterSetExt))
+		writeSizeDataList(writer,this.SequenceParameterSetExt)
+	}
+	return writer.Bytes()
+}
+
+func writeSizeDataList(writer *bytes.Buffer,data *list.List){
+	if data!=nil&&data.Len()>0{
+		for e:=data.Front();e!=nil;e=e.Next(){
+			size:=len(e.Value.([]byte))
+			writer.WriteByte(byte(size>>8))
+			writer.WriteByte(byte(size&0xff))
+			writer.Write(e.Value.([]byte))
+		}
+	}
+}
+
+func readSizeDataList(reader *bytes.Reader,count int)(lst *list.List,err error){
+	lst=list.New()
+	var data []byte
+	defer func() {
+		if err!=nil{
+			lst=nil
+		}
+	}()
+	for i:=0;i<count;i++{
+		data,err=readData(reader,2)
+		if err!=nil{
+			return
+		}
+		size:=(int(data[0])<<8)|(int(data[1]))
+		if size==0{
+			err=errors.New("invalid sps size")
+			return
+		}
+		data,err=readData(reader,size)
+		if err!=nil{
+			return
+		}
+		lst.PushBack(data)
+	}
+
+	return
+}
+
+func readData(reader *bytes.Reader,size int)(data []byte,err error){
+	if size==0{
+		return nil,errors.New("invalid param")
+	}
+	data=make([]byte,size)
+	n,err:=reader.Read(data)
+	if err!=nil{
+		return nil,err
+	}
+	if n!=size{
+		return nil,errors.New("no enough data for read")
+	}
+	return
+}
+
+
+func DecodeAVC(avcData []byte)(avc *AVCDecoderConfigurationRecord,err error){
+	avc=&AVCDecoderConfigurationRecord{}
+	reader:=bytes.NewReader(avcData)
+	var data []byte
+	data,err=readData(reader,6)
+	if err!=nil{
+		return
+	}
+	if data[0]!=1{
+		return avc,errors.New("invalid avc")
+	}
+	avc.ConfigurationVersion=data[0]
+	avc.AVCProfileIndication=data[1]
+	avc.Profile_compatibility=data[2]
+	avc.AVCLevelIndication=data[3]
+	avc.LengthSizeMinusOne=data[4]&3
+	avc.NumOfSequenceParameterSets=int(data[5]&0x1f)
+	avc.SPS,err=readSizeDataList(reader,avc.NumOfSequenceParameterSets)
+	if err!=nil{
+		return
+	}
+	data,err=readData(reader,1)
+	if err!=nil{
+		return
+	}
+	avc.NumOfPictureParameterSets=int(data[0])
+	avc.PPS,err=readSizeDataList(reader,avc.NumOfPictureParameterSets)
+	if err!=nil{
+		return
+	}
+	if avc.AVCProfileIndication==100||avc.AVCProfileIndication==110||
+		avc.AVCProfileIndication==122||avc.AVCProfileIndication==144{
+		data,err=readData(reader,4)
+		if err!=nil{
+			return
+		}
+		avc.Chroma_format_idc=data[0]&3
+		avc.Bit_depth_luma_minus8=data[1]&7
+		avc.Bit_depth_chroma_minus8=data[2]&7
+		avc.NumOfSequenceParameterSetExt=int(data[3])
+		avc.SequenceParameterSetExt,err=readSizeDataList(reader,avc.NumOfSequenceParameterSetExt)
+	}
+
+	return
+}
+
+func (this *AVCDecoderConfigurationRecord)AddSPS(sps []byte)  {
+	if len(sps)==0{
+		return
+	}
+	if nil==this.SPS{
+		this.SPS=list.New()
+	}
+	this.SPS.PushBack(sps)
+	this.NumOfSequenceParameterSets=this.SPS.Len()
+	_,_,_,this.Chroma_format_idc,this.Bit_depth_luma_minus8,this.Bit_depth_chroma_minus8=DecodeSPS(sps)
+}
+
+func (this *AVCDecoderConfigurationRecord)AddPPS(pps []byte){
+	if len(pps)==0{
+		return
+	}
+	if nil==this.PPS{
+		this.PPS=list.New()
+	}
+	this.PPS.PushBack(pps)
+	this.NumOfPictureParameterSets=this.PPS.Len()
+}
+
+func (this *AVCDecoderConfigurationRecord)AddSPSExt(spsExt []byte){
+	if len(spsExt)==0{
+		return
+	}
+	if nil==this.SequenceParameterSetExt{
+		this.SequenceParameterSetExt=list.New()
+	}
+	this.SequenceParameterSetExt.PushBack(spsExt)
+	this.NumOfSequenceParameterSetExt=this.SequenceParameterSetExt.Len()
+}
--- format/H264/H264.go
+++ format/H264/H264.go
@@ -1,6 +1,9 @@
 package H264
 
-import "container/list"
+import (
+	"bytes"
+	"github.com/panda-media/muxer-fmp4/utils"
+)
 
 const (
 	NAL_SLICE           = 1
@@ -19,19 +22,148 @@ const (
 	NAL_AUXILIARY_SLICE = 19
 )
 
-type AVCDecoderConfigurationRecord struct {
-	ConfigurationVersion       byte //aways 1
-	AVCProfileIndication       byte //sps[1]
-	Profile_compatibility      byte //sps[2]
-	AVCLevelIndication         byte //sps[3]
-	LengthSizeMinusOne         byte //
-	NumOfSequenceParameterSets int
-	SPS                        *list.List
-	NumOfPictureParameterSets int
-	PPS *list.List
-	Chroma_format byte
-	Bit_depth_luma_minus8 byte
-	Bit_depth_chroma_minus8 byte
-	NumOfSequenceParameterSetExt int
-	SequenceParameterSetExt *list.List
+
+func emulation_prevention(nal []byte) []byte {
+	buf := bytes.Buffer{}
+	for i := 0; i < len(nal); i++ {
+		if i+2 < len(nal) {
+			if 0 == (nal[i]^0x00)+(nal[i+1]^0x00)+(nal[i+2]^0x03) {
+				buf.WriteByte(nal[i])
+				i++
+				buf.WriteByte(nal[i])
+				i++
+				continue
+			}
+		}
+		buf.WriteByte(nal[i])
+	}
+	return buf.Bytes()
+}
+
+func DecodeSPS(sps []byte) (width, height, fps int, chroma_format_idc, bit_depth_luma_minus8, bit_depth_chroma_minus8 byte) {
+
+	data := emulation_prevention(sps)
+
+	bit := &utils.BitReader{}
+	bit.Init(data)
+	bit.ReadBits(8)
+	profile_idc := bit.ReadBits(8)
+	bit.ReadBits(16)
+	bit.ReadExponentialGolombCode()
+
+	if profile_idc == 100 || profile_idc == 110 ||
+		profile_idc == 122 || profile_idc == 244 ||
+		profile_idc == 44 || profile_idc == 83 ||
+		profile_idc == 86 || profile_idc == 118 {
+		chroma_format_idc = byte(bit.ReadExponentialGolombCode())
+		if chroma_format_idc == 3 {
+			bit.ReadBit()
+		}
+		bit_depth_luma_minus8 = byte(bit.ReadExponentialGolombCode())   //bit_depth_luma_minus
+		bit_depth_chroma_minus8 = byte(bit.ReadExponentialGolombCode()) //bit_depth_chroma_minus8
+		bit.ReadBit()
+		seq_scaling_matrix_present_flag := bit.ReadBit()
+		if seq_scaling_matrix_present_flag != 0 {
+			for i := 0; i < 8; i++ {
+				seq_scaling_list_present_flag := bit.ReadBit()
+				if seq_scaling_list_present_flag != 0 {
+					var sizeOfScalingList int
+					if i < 6 {
+						sizeOfScalingList = 16
+					} else {
+						sizeOfScalingList = 64
+					}
+					lastScale := 8
+					nextScale := 8
+					for j := 0; j < sizeOfScalingList; j++ {
+						delta_scale := bit.ReadSE()
+						nextScale = (lastScale + delta_scale + 256) % 256
+					}
+					if nextScale == 0 {
+						lastScale = lastScale
+					} else {
+						lastScale = nextScale
+					}
+				}
+			}
+		}
+	}
+
+	bit.ReadExponentialGolombCode()
+	pic_order_cnt_type := bit.ReadExponentialGolombCode()
+	if 0 == pic_order_cnt_type {
+		bit.ReadExponentialGolombCode()
+	} else if 1 == pic_order_cnt_type {
+		bit.ReadBit()
+		bit.ReadSE()
+		bit.ReadSE()
+		num_ref_frames_in_pic_order_cnt_cycle := bit.ReadExponentialGolombCode()
+		for i := 0; i < num_ref_frames_in_pic_order_cnt_cycle; i++ {
+			bit.ReadSE()
+		}
+	}
+
+	bit.ReadExponentialGolombCode()
+	bit.ReadBit()
+	pic_width_in_mbs_minus1 := bit.ReadExponentialGolombCode()
+	pic_height_in_map_units_minus1 := bit.ReadExponentialGolombCode()
+	frame_mbs_only_flag := bit.ReadBit()
+	if frame_mbs_only_flag == 0 {
+		bit.ReadBit()
+	}
+	bit.ReadBit()
+	frame_cropping_flag := bit.ReadBit()
+	var frame_crop_left_offset int
+	var frame_crop_right_offset int
+	var frame_crop_top_offset int
+	var frame_crop_bottom_offset int
+	if frame_cropping_flag != 0 {
+		frame_crop_left_offset = bit.ReadExponentialGolombCode()
+		frame_crop_right_offset = bit.ReadExponentialGolombCode()
+		frame_crop_top_offset = bit.ReadExponentialGolombCode()
+		frame_crop_bottom_offset = bit.ReadExponentialGolombCode()
+	}
+
+	width = ((pic_width_in_mbs_minus1 + 1) * 16) - frame_crop_bottom_offset*2 - frame_crop_top_offset*2
+	height = ((2 - frame_mbs_only_flag) * (pic_height_in_map_units_minus1 + 1) * 16) - (frame_crop_right_offset * 2) - (frame_crop_left_offset * 2)
+
+	vui_parameters_present_flag := bit.ReadBit()
+	if vui_parameters_present_flag != 0 {
+		aspect_ratio_info_present_flag := bit.ReadBit()
+		if aspect_ratio_info_present_flag != 0 {
+			aspect_ratio_idc := bit.ReadBits(8)
+			if aspect_ratio_idc == 255 {
+				bit.ReadBits(16)
+				bit.ReadBits(16)
+			}
+		}
+		overscan_info_present_flag := bit.ReadBit()
+		if 0 != overscan_info_present_flag {
+			bit.ReadBit()
+		}
+		video_signal_type_present_flag := bit.ReadBit()
+		if video_signal_type_present_flag != 0 {
+			bit.ReadBits(3)
+			bit.ReadBit()
+			colour_description_present_flag := bit.ReadBit()
+			if colour_description_present_flag != 0 {
+				bit.ReadBits(8)
+				bit.ReadBits(8)
+				bit.ReadBits(8)
+			}
+		}
+		chroma_loc_info_present_flag := bit.ReadBit()
+		if chroma_loc_info_present_flag != 0 {
+			bit.ReadExponentialGolombCode()
+			bit.ReadExponentialGolombCode()
+		}
+
+		timing_info_present_flag := bit.ReadBit()
+		if 0 != timing_info_present_flag {
+			num_units_in_tick := bit.ReadBits(32)
+			time_scale := bit.ReadBits(32)
+			fps = time_scale / (2 * num_units_in_tick)
+		}
+	}
+	return
 }
